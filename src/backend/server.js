6566937856bdb61bf560ab540f40d8286a530d3d
Jesper Carlsson <jespercarlsson@Jespers-MacBook-Air.local>
2016-04-18 16:11:50 +0200

    Testing some stuff

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,10 +4,13 @@ import _ from 'lodash';
 var app = express();
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/hello', (req, res) => {
-  res.send('HELLO');
+app.get('/hello/:name', (req, res)=> {
+  var name = req.params.name;
+  res.send('Fuck you ' + name);
 });
 
 app.listen(3000, () => {
   console.log('Listening :3000');
 });
+
+
