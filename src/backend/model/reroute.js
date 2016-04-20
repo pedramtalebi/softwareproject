3a9f90019b234188bc9d93153f1746e42c4938fe
sebastian <me@sebastianlund.se>
2016-04-20 11:14:19 +0200

    added simple pre to reroute

--- src/backend/model/reroute.js
+++ src/backend/model/reroute.js
@@ -1,11 +1,27 @@
 var mongoose = require('mongoose');
+var uuid = require('node-uuid');
 
 var rerouteSchema = new mongoose.Schema({
-    id: {type: String, unique: true},
-    mapPath: {type: String},
-    
-    created_at: Date,
-    updated_at: Date
+  id: {type: String, unique: true},
+  mapPath: {type: String},
+
+  created_at: Date,
+  updated_at: Date
+});
+
+rerouteSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+  
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  
+  if (!this.id) {
+    this.id = uuid.v4();
+  }
+  
+  next();
 });
 
 var Reroute = mongoose.model('reroute', rerouteSchema);
