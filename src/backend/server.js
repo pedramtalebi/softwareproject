33d9948c13ac212bccc3b20bca3c66e29e4f17ce
Pedram Talebi <pedram.talebi18@gmail.com>
2016-04-27 12:33:42 +0200

    Able to render frontend views

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,12 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
 
 import rerouteApi from './routes/reroute-api';
 import lineApi from './routes/line-api';
 import busApi from './routes/bus-api';
 
+var path = require('path');
 
 mongoose.connect('mongodb://localhost/database');
 //mongoose.connect('mongodb://mongo/softwareproject');
@@ -12,16 +14,17 @@ mongoose.connect('mongodb://localhost/database');
 var app = express();
 app.use('/', express.static(__dirname + '/public'));
 
+// log all requests to the console 
+app.use(morgan('dev'));
+
 app.use(rerouteApi);
 app.use(lineApi);
 app.use(busApi);
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + 'app/views/index.html'));
+	res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(3000, () => {
   console.log('Listening :3000');
 });
-
-
