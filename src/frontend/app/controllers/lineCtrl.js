c09b11297db3c2f6103d4b2715801f8c05dc50b0
Amanda Belfrage <amanda.belfrage@gmail.com>
2016-04-29 11:23:29 +0200

    fixing linepage.html

--- src/frontend/app/controllers/lineCtrl.js
+++ src/frontend/app/controllers/lineCtrl.js
@@ -3,5 +3,12 @@ angular.module('lineCtrl', [])
 .controller('lineController', ['$scope','$routeParams', function($scope, $routeParams) {
     // Id nummer för specifik linje
     $scope.id = $routeParams.id;
+//Dummie reroutes
+    $scope.reroutes = [
+    	{name: 'Korsvägen'},
+    	{name: 'Avenyn'},
+    	{name: 'Brunnsparken'},
+    	{name: 'Domkyrkan'}
+    ];
      
 }]);
