c78c3642cd59ba92ccc6f6461743d1985048c31a
Pedram Talebi <pedram.talebi18@gmail.com>
2016-05-04 09:40:03 +0200

    fixing linepage

--- src/frontend/app/controllers/mainCtrl.js
+++ src/frontend/app/controllers/mainCtrl.js
@@ -28,9 +28,7 @@ angular.module('mainCtrl', [])
     ];
     
     NgMap.getMap().then(function(map) {
-        console.log(map.getCenter());
-        console.log('markers', map.markers);
-        console.log('shapes', map.shapes);
+       
     });
      
 }]);
